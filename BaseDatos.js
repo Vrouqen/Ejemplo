0ca1e2aba929b5b67654fe144b889127dcadf307
Vrouqen <luiselomi@outlook.com>
2024-08-08 19:41:18 -0500

    App

---
+++ BaseDatos.js
@@ -0,0 +1,10 @@
+var apiKey = "AIzaSyBKDWgLz5FNK_5QedckuKAo_lZ-QPn_FT4";
+var authDomain = "ejemplo-b9698.firebaseapp.com";
+var databaseURL = "";
+var projectId = "ejemplo-b9698";
+var storageBucket = "ejemplo-b9698.appspot.com";
+var messagingSenderId = "254079836685";
+var appId = "1:254079836685:web:d4903f89ed5e744f28eb44";
+var measurementId = "G-VB3T3YHZJJ";
+
+var ApiGPT = "";
